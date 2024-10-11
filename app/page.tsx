4e2bf8179b97a5987ad2f18da83d81f4d5515e49
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2024-10-11 22:01:38 +0530

    user button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
+import UserButton from "@/features/auth/components/user-button";
 
 export default function Home() {
   const { data, isLoading } = useCurrent();
   console.log("data, isLoading", data, isLoading);
 
-  const { mutate } = useLogout();
   return (
     <div className="grid text-blue-600 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello world 2<Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
