9bad721a5f9cd1231cb4a7e4c2c1e44c5378b4f3
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2024-10-11 23:32:02 +0530

    large screen side bar added

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
+import React, { ReactNode } from "react";
+
+interface DashboardLayoutProps {
+  children: ReactNode;
+}
+
+function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="min-h-screen ">
+      <div className="flex w-full h-full">
+        <div className="fixed left-0 top-0 lg:block lg:w-[264px] h-full overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="lg:pl-[264px] w-full">
+          <div className="max-w-screen-2xl mx-auto h-full">
+            <Navbar />
+            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
