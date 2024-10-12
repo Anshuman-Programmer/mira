b9f97babf19291fe9de90385802c0c0bd02c6e4b
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2024-10-12 13:39:12 +0530

    workspace selecto comp added

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "@/components/workspace-switcher";
 
 export const Sidebar = () => {
   return (
@@ -10,6 +11,8 @@ export const Sidebar = () => {
       <Link href="/">
         <Image src={"/logo.svg"} alt="logo" width={164} height={56} />
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
